5b3d02a17056cd018e07ca3e474b02cbce2f2364
Nikhil Kadyan <nikhilkadyan@icloud.com>
2022-09-19 17:26:16 +0530

    moved to next JS

---
+++ pages/github.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout title={'Github Tracker'}>
+      Hey There
+    </Layout>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import styles from '../styles/Home.module.css'
+import { FaGithub, FaTwitter, FaLinkedinIn, FaInstagram } from 'react-icons/fa';
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className={styles.content}>
+        <div className={styles.title}>👋 I'm Nikhil Kadyan</div>
+        <div className={styles.subtitle}>`A Backend developer`</div>
+        <div className={styles.social}>
+          <a href='https://github.com/nikhilkadyan' target={'_blank'} rel={'noreferrer'}><FaGithub /></a>
+          <a href='https://twitter.com/kadyanikhil' target={'_blank'} rel={'noreferrer'}><FaTwitter /></a>
+          <a href='https://linkedin.com/in/nikhilkadyan/' target={'_blank'} rel={'noreferrer'}><FaLinkedinIn /></a>
+          <a href='https://instagram.com/nikhilkadyan' target={'_blank'} rel={'noreferrer'}><FaInstagram /></a>
+        </div>
+        <div className={styles.actions}>
+          <a href='/nikhil_kadyan_cv.pdf' className={styles.button} target={'_blank'} rel={'noreferrer'}>Résumé</a>
+          <a href='mailto:nikhilkadyan@icloud.com' className={styles.button}>Email me</a>
+        </div>
+      </div>
+    </Layout>
+  )
+}
