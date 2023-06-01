91c4ebf3a42baa308ff38632a85799eaf703b04c
Nikhil Kadyan <nikhilkadyan@Nikhils-MacBook-Air.local>
2023-06-02 01:03:25 +0530

    Switched to static code

--- pages/github.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-export default function Github() {
-  return (
-    <Layout title={'Github Tracker'}>
-      Come back later. Still under construction.
-    </Layout>
-  )
-}
